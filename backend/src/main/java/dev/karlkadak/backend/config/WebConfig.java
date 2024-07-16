691e2b232cabe49a8dffffe0be50657e733022dc
Karl Kadak <kadakkarl@gmail.com>
2024-07-16 18:38:09 +0300

    backend: add CORS configuration to accept HTTP requests from localhost:3000

---
+++ backend/src/main/java/dev/karlkadak/backend/config/WebConfig.java
@@ -0,0 +1,28 @@
+package dev.karlkadak.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration class to set up global CORS settings, ensures the backend application allows cross-origin requests from
+ * the React frontend
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000").
+                        allowedMethods("GET", "POST", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
